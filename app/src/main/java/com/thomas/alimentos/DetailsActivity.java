20314c36b6e418ba3b8842b6ffa7a0c3beb0b7fe
thomas-rodrigues <thomasmichels.bnu@gmail.com>
2021-02-17 13:46:02 -0300

    Mapeando elementos e organizando views

---
+++ app/src/main/java/com/thomas/alimentos/DetailsActivity.java
@@ -0,0 +1,28 @@
+package com.thomas.alimentos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        // Elementos de Interface
+        this.mViewHolder.mTextName = findViewById(R.id.text_name);
+        this.mViewHolder.mTextCalories = findViewById(R.id.text_calories);
+
+
+    }
+
+    private static class ViewHolder {
+        TextView mTextName;
+        TextView mTextCalories;
+    }
+}
