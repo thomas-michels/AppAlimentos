0a06486ae33deb70010c92be8b6f6ab4e535a4b9
thomas-rodrigues <thomasmichels.bnu@gmail.com>
2021-02-19 14:46:34 -0300

    App de Alimentos v1

--- app/src/main/java/com/thomas/alimentos/ui/DetailsActivity.java
+++ app/src/main/java/com/thomas/alimentos/ui/DetailsActivity.java
@@ -1,21 +1,31 @@
 package com.thomas.alimentos.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.thomas.alimentos.R;
+import com.thomas.alimentos.business.FoodBusiness;
 import com.thomas.alimentos.constants.FoodConstants;
+import com.thomas.alimentos.entity.FoodEntity;
 
 public class DetailsActivity extends AppCompatActivity {
 
     private ViewHolder mViewHolder = new ViewHolder();
+    private int mId = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
 
         // Elementos de Interface
         this.mViewHolder.mTextName = findViewById(R.id.text_name);
@@ -24,11 +34,31 @@ public class DetailsActivity extends AppCompatActivity {
         this.getData();
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        // Pegando evento de click no botão de voltar
+        if (item.getItemId() == android.R.id.home) {
+
+            // Fecha a activity atual
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private void getData() {
         Bundle bundle = getIntent().getExtras();
 
         if (bundle != null) {
-            int id = bundle.getInt(FoodConstants.FOOD_ID);
+            this.mId = bundle.getInt(FoodConstants.FOOD_ID);
+
+            // Carrega dados
+            FoodEntity foodEntity = new FoodBusiness().get(this.mId);
+
+            // Seta variaveis para aparecer na tela
+            
+            this.mViewHolder.mTextName.setText(foodEntity.getName());
+            this.mViewHolder.mTextCalories.setText(String.valueOf(foodEntity.getCalories()));
         }
     }
 
