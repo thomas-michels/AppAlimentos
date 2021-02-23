608745914297fcdee87df65967ac0101b5fc2d05
thomas-rodrigues <thomasmichels.bnu@gmail.com>
2021-02-23 09:23:38 -0300

    Adicionando novas informações para os alimentos

--- app/src/main/java/com/thomas/alimentos/entity/FoodEntity.java
+++ app/src/main/java/com/thomas/alimentos/entity/FoodEntity.java
@@ -5,11 +5,17 @@ public class FoodEntity {
     private int id;
     private String name;
     private int calories;
+    private int quantity;
+    private String unit;
+    private String description;
 
-    public FoodEntity(int id, String name, int calories) {
+    public FoodEntity(int id, String name, int calories, int quantity, String unit, String description) {
         this.id = id;
         this.name = name;
         this.calories = calories;
+        this.quantity = quantity;
+        this.unit = unit;
+        this.description = description;
     }
 
     public int getId() {
@@ -34,5 +40,29 @@ public class FoodEntity {
 
     public void setCalories(int calories) {
         this.calories = calories;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
